19d798090c4b4883b61e7ebafb7dfa6a643f4232
Eveline Visee <eveline.visee@gmail.com>
2020-09-30 20:40:02 +0200

    Add mypy annotations

--- lsys.py
+++ lsys.py
@@ -1,23 +1,26 @@
+from typing import Dict, List
+
+
 class LSystem:
-    def __init__(self, rules, start):
+    def __init__(self, rules: Dict[str, str], start: str) -> None:
         self.iteration = 0
         self.state = [start]
         self.rules = rules
 
-    def apply_rule(self, x):
+    def apply_rule(self, x: str) -> str:
         return self.rules.get(x, x)
 
-    def iterate(self):
-        new_state = []
+    def iterate(self) -> None:
+        new_state = []  # type: List
         for x in self.state:
             new_state += self.apply_rule(x)
         self.state = new_state
         self.iteration += 1
     
-    def __str__(self):
+    def __str__(self) -> str:
         return "{}: {}".format(self.iteration, " ".join(self.state))
 
-    def iterate_many(self, num):
+    def iterate_many(self, num: int) -> None:
         for i in range(num):
             self.iterate()
 
